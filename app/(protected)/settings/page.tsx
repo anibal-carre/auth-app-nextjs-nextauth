5e9a6c46005fc50b168ada63d7edb754ee63f169
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-04-15 15:15:28 -0300

    signin and signout

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,20 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 const SettingsPage = async () => {
   const session = await auth();
   return (
     <div>
       <span>{JSON.stringify(session)}</span>
+
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+      </form>
     </div>
   );
 };

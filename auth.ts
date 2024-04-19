53e0b03cc911db8ca782555e40997c50508fada4
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-04-19 13:59:09 -0300

    work in Oauth

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
@@ -11,12 +13,26 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ token, session }) {
+      console.log({ sessionToken: token });
+
       if (token.sub && session.user) {
         session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
       }
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     },
   },

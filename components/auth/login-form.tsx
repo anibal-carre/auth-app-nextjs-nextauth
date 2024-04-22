da2e46d1b82eeec1f4890e64c633a5ea5f4f7c6c
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-04-22 12:35:29 -0300

    google and github oauth added

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -6,6 +6,8 @@ import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { CardWrapper } from "./card-wrapper";
 
+import { useSearchParams } from "next/navigation";
+
 import {
   Form,
   FormControl,
@@ -23,6 +25,12 @@ import { FormSuccess } from "@/components/form-success";
 import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with different provider!"
+      : "";
+
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
 
@@ -43,8 +51,9 @@ export const LoginForm = () => {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        //TO DO: Add when we add 2FA
+        // setSuccess(data?.success);
       });
     });
   };
@@ -97,7 +106,7 @@ export const LoginForm = () => {
             />
           </div>
 
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
 
           <Button disabled={isPending} type="submit" className="w-full">
